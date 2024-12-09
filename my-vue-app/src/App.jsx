4289a0a5bb792e5e1bf5f4c6402acb85721c33e6
HarshavKoka <kokaharsha2203@gmail.com>
2024-12-09 23:56:15 +0530

    mini change

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
   const [count, setCount] = useState(0)
   const inc = () => {
-    setCount((prev) => prev + 5)
+    setCount((prev) => prev + 6)
   }
   console.log(count)
   return (
